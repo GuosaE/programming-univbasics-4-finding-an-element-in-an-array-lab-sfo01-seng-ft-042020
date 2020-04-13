4c379c4a8db0fbaddb5e037a9aac68ea05ae79f6
Guosa Edebiri <gedebiri@gmail.com>
2020-04-13 04:05:27 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,15 +1,14 @@
 def find_element_index(array, value_to_find)
   
   count = 0 
+  found_value_index = nil 
   
-  while array[count] do
+  while count < array.length.do
     
-    if (array[count] == value_to_find)
-      break
-    else
-      return nil
-    
+    if array[count] == value_to_find
+      found_value_index = count
+    end
     count += 1 
   end
-  count
+  found_value_index
 end
